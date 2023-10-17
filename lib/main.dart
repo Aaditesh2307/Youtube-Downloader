26033d7e0f64ca02e04a426a9d52d9f783ed8135
Aaditesh2307 <118682176+Aaditesh2307@users.noreply.github.com>
2023-10-17 07:23:40 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -26,112 +26,112 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
         home: Scaffold(
             body: Center(
-              child: Container(
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.fitHeight,
-                      image: AssetImage('assets/bg.jpg'),
-                    ),
-                  ),
-                  child: Scaffold(
-                    backgroundColor: Colors.transparent,
+      child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fitHeight,
+              image: AssetImage('assets/bg.jpg'),
+            ),
+          ),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
 
-                    // appBar: AppBar(
-                    body: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 65.0),
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const Text.rich(
-                                TextSpan(
-                                  text: 'BY',
-                                  style: TextStyle(color: Colors.white),
-                                  // default text style
-                                  children: <TextSpan>[
-                                    TextSpan(
-                                        text: ' PREMVED DHOTE ',
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontStyle: FontStyle.italic)),
-                                    // TextSpan(
-                                    //     text: 'PREMVED DHOTE',
-                                    //     style: TextStyle(
-                                    //         color: Colors.white,
-                                    //         fontWeight: FontWeight.bold)),
-                                    // const TextStyle(color: Colors.white),
-                                  ],
-                                ),
-                              ),
-                              SizedBox(
-                                child: TextFormField(
-                                  controller: urlController,
-                                  decoration: const InputDecoration(
-                                    hintText: "Enter Youtube URL",
-                                    hintStyle: TextStyle(color: Colors.white),
-                                  ),
-                                  style: const TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              qualityButton(quality: "360p"),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              qualityButton(quality: "720p"),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              qualityButton(quality: "1080p"),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              ElevatedButton(
-                                child: const Text(
-                                  "Download",
-                                  style: TextStyle(color: Colors.white, fontSize: 20),
-                                ),
-                                style: ButtonStyle(
-                                  backgroundColor:
-                                  MaterialStateProperty.all<Color>(Colors.black),
-                                  elevation: MaterialStateProperty.all(10),
-                                  padding:
-                                  MaterialStateProperty.all<EdgeInsetsGeometry>(
-                                    const EdgeInsets.symmetric(
-                                        vertical: 10, horizontal: 60),
-                                  ),
-                                ),
-                                // shape:MaterialStateProperty.all(
-                                //   RoundedRectangleBorder(
-                                //     borderRadius: BorderRadius.circular(13),
-                                //   ),
-                                // ),
-                                onPressed: () async {
-                                  link =
-                                  await FlutterYoutubeDownloader.extractYoutubeLink(
-                                    urlController.text,
-                                    itag,
-                                  );
-                                  if (link == "Failed to get itag URL") {
-                                    setState(() {
-                                      error = "Invalid Youtube URL";
-                                    });
-                                  } else {
-                                    FlutterYoutubeDownloader.downloadVideo(
-                                        urlController.text.trim(),
-                                        "Youtube Video ",
-                                        itag);
-                                  }
-                                },
-                              ),
-                            ])),
-                  )),
+            // appBar: AppBar(
+            body: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 65.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text.rich(
+                        TextSpan(
+                          text: 'BY',
+                          style: TextStyle(color: Colors.white),
+                          // default text style
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: ' AADITESH KADU ',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontStyle: FontStyle.italic)),
+                            // TextSpan(
+
+                            //     style: TextStyle(
+                            //         color: Colors.white,
+                            //         fontWeight: FontWeight.bold)),
+                            // const TextStyle(color: Colors.white),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        child: TextFormField(
+                          controller: urlController,
+                          decoration: const InputDecoration(
+                            hintText: "Enter Youtube URL",
+                            hintStyle: TextStyle(color: Colors.white),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      qualityButton(quality: "360p"),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      qualityButton(quality: "720p"),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      qualityButton(quality: "1080p"),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      ElevatedButton(
+                        child: const Text(
+                          "Download",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                          elevation: MaterialStateProperty.all(10),
+                          padding:
+                              MaterialStateProperty.all<EdgeInsetsGeometry>(
+                            const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 60),
+                          ),
+                        ),
+                        // shape:MaterialStateProperty.all(
+                        //   RoundedRectangleBorder(
+                        //     borderRadius: BorderRadius.circular(13),
+                        //   ),
+                        // ),
+                        onPressed: () async {
+                          link =
+                              await FlutterYoutubeDownloader.extractYoutubeLink(
+                            urlController.text,
+                            itag,
+                          );
+                          if (link == "Failed to get itag URL") {
+                            setState(() {
+                              error = "Invalid Youtube URL";
+                            });
+                          } else {
+                            FlutterYoutubeDownloader.downloadVideo(
+                                urlController.text.trim(),
+                                "Youtube Video ",
+                                itag);
+                          }
+                        },
+                      ),
+                    ])),
+          )),
 // Text (
 //   error,
 //   style: TextStyle(fontSize: 22, color: Colors.white, fontWeight: FontWeight.bold)
 // )
-            )));
+    )));
   }
 
   ElevatedButton qualityButton({required String quality}) {
